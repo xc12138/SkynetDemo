ec86c1f965f0982d3c7926ad1e9548d51e79d2a2
xc12138 <1735411768@qq.com>
2025-04-10 01:21:41 +0800

    Update main.lua

--- service/main.lua
+++ service/main.lua
@@ -5,17 +5,17 @@ skynet.start(function()
     skynet.error("[start main] hello world")
 
     -- 启动打工服务
-    local worker = skynet.newservice("worker","xuchao",1)
-    skynet.name("worker",worker)
-    -- 启动买猫粮服务
-    -- local buy1 = skynet.newservice("buy","buy",1)
+    -- local worker = skynet.newservice("worker","xuchao",1)
+    -- skynet.name("worker",worker)
+    -- -- 启动买猫粮服务
+    -- -- local buy1 = skynet.newservice("buy","buy",1)
 
-    -- 开始工作
-    skynet.send(worker,"lua","start_work")
-    -- 主服务休息2秒
-    skynet.sleep(200)
-    -- 停止工作
-    skynet.send(worker,"lua","stop_work")
+    -- -- 开始工作
+    -- skynet.send(worker,"lua","start_work")
+    -- -- 主服务休息2秒
+    -- skynet.sleep(200)
+    -- -- 停止工作
+    -- skynet.send(worker,"lua","stop_work")
 
     -- 买猫粮
     -- skynet.send(buy,"lua","buy")
