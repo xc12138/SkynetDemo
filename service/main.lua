ceef1cdcaf55fc65251a557425cdc4d55895b5cd
xc12138 <1735411768@qq.com>
2025-04-10 12:11:47 +0800

    Update main.lua

--- service/main.lua
+++ service/main.lua
@@ -2,23 +2,36 @@ local skynet = require "skynet"
 require "skynet.manager"
 
 skynet.start(function()
-    skynet.error("[start main] hello world")
+    -- skynet.error("[start main] hello world")
 
-    -- 启动打工服务
-    local worker = skynet.newservice("worker","xuchao",1)
-    skynet.name("worker",worker)
-    -- 启动买猫粮服务
-    local buy1 = skynet.newservice("buy","buy",1)
+    -- -- 启动打工服务
+    -- local worker = skynet.newservice("worker","xuchao",1)
+    -- skynet.name("worker",worker)
+    -- -- 启动买猫粮服务
+    -- local buy1 = skynet.newservice("buy","buy",1)
 
-    -- 开始工作
-    skynet.send(worker,"lua","start_work")
-    -- 主服务休息2秒
-    skynet.sleep(200)
-    -- 停止工作
-    skynet.send(worker,"lua","stop_work")
+    -- -- 开始工作
+    -- skynet.send(worker,"lua","start_work")
+    -- -- 主服务休息2秒
+    -- skynet.sleep(200)
+    -- -- 停止工作
+    -- skynet.send(worker,"lua","stop_work")
 
-    -- 买猫粮
-    skynet.send(buy1,"lua","buy")
-
+    -- -- 买猫粮
+    -- skynet.send(buy1,"lua","buy")
+	skynet.error("Server start")
+	skynet.uniqueservice("protoloader")
+	if not skynet.getenv "daemon" then
+		local console = skynet.newservice("console")
+	end
+	skynet.newservice("debug_console",8000)
+	skynet.newservice("simpledb")
+	local watchdog = skynet.newservice("watchdog")
+	local addr,port = skynet.call(watchdog, "lua", "start", {
+		port = 8888,
+		maxclient = max_client,
+		nodelay = true,
+	})
+	skynet.error("Watchdog listen on " .. addr .. ":" .. port)
     skynet.exit()
 end)
